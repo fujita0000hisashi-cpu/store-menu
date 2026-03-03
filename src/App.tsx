09a9dc321d750222af4db023a4723513097dcad0
fujita0000hisashi-cpu <fujita0000hisashi@gmail.com>
2026-03-03 14:09:08 +0900

    feat/ implement hero layout with images (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Header } from "./components/Header";
+import { Header } from "./components/Header/Header";
 import { Menu } from "./components/Menu";
 import { About } from "./components/About";
 import { Location } from "./components/Location";
