98a4bd87581c4890c74f9ad302e7974da73e5dbf
fujita <fujita0000hisashi@gmail.com>
2026-03-06 13:55:05 +0900

    feat: implement about section layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Header } from "./components/Header/Header";
 import { Menu } from "./components/Menu/Menu";
-import { About } from "./components/About";
+import { About } from "./components/About/About";
 import { Location } from "./components/Location";
 import { Footer } from "./components/Footer";
 
--- src/components/About.tsx
+++
@@ -1,9 +0,0 @@
-export function About() {
-  return (
-    <section id="about" className="about">
-      <div className="about-inner">
-        <h2 className="section-title">About</h2>
-      </div>
-    </section>
-  );
-}
